6ec66c23d3bf79ef1e567ff767f9fc0fbed4bf3f
starfield2077 <143923965+starfield2077@users.noreply.github.com>
2024-02-22 18:03:19 +0800

    Create Linked List.py

---
+++ Linked List.py
@@ -0,0 +1,104 @@
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+
+        while itr:
+            llstr += str(itr.data) + '-->'
+            itr = itr.next
+
+        print(llstr)
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def insert_after_value(self, data_after, data_to_insert):
+        itr = self.head
+        while itr.next:
+            if itr.data == data_after:
+                return itr.next
+            itr = itr.next
+        node = Node(data_to_insert, itr.next)
+
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+    def remove_at(self, index):
+        if index < 0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid Index")
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert_at_begining(5)
+    ll.insert_at_begining((89))
+    ll.insert_at_end(79)
+    ll.print()
+    ll1 = LinkedList()
+    ll1.insert_values(["banana","mango","grape","orange"])
+    ll1.print()
+    print("length is: " + str(ll1.get_length()))
+    ll1.remove_at(1)
+    ll1.insert_at(2,"apple")
+    ll1.print()
+
