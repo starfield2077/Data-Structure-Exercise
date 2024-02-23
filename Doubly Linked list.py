3158ff317ea3745a06267d79d736ef8a666fd9ed
starfield2077 <143923965+starfield2077@users.noreply.github.com>
2024-02-23 16:01:26 +0800

    Create Doubly Linked list.py

---
+++ Doubly Linked list.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data = None, next = None, prev = None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+class doublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_forward(self):
+
+
