2a6e2094b169c7a4210f5e822903f41f743c2f2d
starfield2077 <143923965+starfield2077@users.noreply.github.com>
2024-03-09 21:43:00 +0800

    Update HashTable1.py

--- HashTable1.py
+++ HashTable1.py
@@ -7,5 +7,21 @@ with open("nyc_weather.csv", 'r') as myFile:
         dic.update({data[index][0]:int(data[index][1])})
 
 aver_temp = 0
-for i in range(7):
-    aver_temp += dic[i]
+max_temp = 0
+key = 'Jan '
+day = 0
+print(dic)
+for i in range(1,8):
+    print("Jan " + str(i) + "'s temperature is " + str(dic[key + str(i)]))
+    aver_temp += dic[key + str(i)] / 7
+print("average temperature of first week in January is: " + str(aver_temp) + '\n')
+
+for i in range(1,11):
+    if dic[key + str(i)] >= max_temp:
+        max_temp = dic[key + str(i)]
+        day = str(i)
+
+print("maximum temperature in first 10 days in January is: " + str(max_temp) + " and it's on: " + str(key + day) + '\n')
+
+print("temperature on Jan 9 is: " + str(dic['Jan 9']))
+print("temperature on Jan 4 is: " + str(dic['Jan 4']))
