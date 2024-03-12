ba5ea318ae8c495d4c938b0b37eff001e7112511
starfield2077 <143923965+starfield2077@users.noreply.github.com>
2024-03-12 22:37:24 +0800

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Stack:
+    def __init__(self):
+        self.container = deque()
+    def push(self, val):
+        self.container.append(val)
+    def pop(self):
+        return self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+    def size(self):
+        return len(self.container)
+
+    def is_empty(self):
+        return len(self.container) == 0
+
+s = Stack()
+
+def reverse_string(string):
+    new_string = ""
+    for c in string:
+        s.push(c);
+    while (not s.is_empty()):
+        new_string += s.pop();
+    return new_string
+
+print(reverse_string("We will conquere COVID-19"))
