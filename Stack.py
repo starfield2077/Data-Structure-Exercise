97cfb97d172fc2b69e95c07767e3fe47826d1818
starfield2077 <143923965+starfield2077@users.noreply.github.com>
2024-03-13 12:08:29 +0800

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,9 +1,13 @@
 from collections import deque
+
+
 class Stack:
     def __init__(self):
         self.container = deque()
+
     def push(self, val):
         self.container.append(val)
+
     def pop(self):
         return self.container.pop()
 
@@ -17,7 +21,6 @@ class Stack:
         return len(self.container) == 0
 
 
-
 def reverse_string(string):
     s = Stack()
     new_string = ""
@@ -27,7 +30,46 @@ def reverse_string(string):
         new_string += s.pop();
     return new_string
 
+
 print(reverse_string("We will conquere COVID-19"))
 
-def is_balenced(string):
-    stack = Stack()
+
+def is_balanced(string):
+    s = Stack()
+    for c in string:
+        if c == '(':
+            s.push(c)
+        elif c == '[':
+            s.push(c)
+        elif c == '{':
+            s.push(c)
+        elif c == ')':
+            if not s.is_empty() and s.peek() == '(':
+                s.pop()
+            else:
+                s.push(c)
+        elif c == '}':
+            if not s.is_empty() and s.peek() == '{':
+                s.pop()
+            else:
+                s.push(c)
+        elif c == ']':
+            if not s.is_empty() and s.peek() == '[':
+                s.pop()
+            else:
+                s.push(c)
+        else:
+            continue
+    return s.is_empty()
+
+
+res = is_balanced("({a+b})")
+print(res)
+res1 = is_balanced("))((a+b}{")
+print(res1)
+res2 = is_balanced("((a+b))")
+print(res2)
+res3 = is_balanced("))")
+print(res3)
+res4 = is_balanced("[a+b]*(x+2y)*{gg+kk}")
+print(res4)
